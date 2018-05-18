4828b96b296870c13ff6dc73924308f37ae123d7
lmtthws <lmtthws@gmail.com>
2018-05-18 06:48:55 -0500

    now we have a slow running page

--- web_server/src/main.rs
+++ web_server/src/main.rs
@@ -2,6 +2,8 @@ use std::net::TcpListener;
 use std::net::TcpStream;
 use std::io::prelude::*;
 use std::fs::File;
+use std::thread;
+use std::time::Duration;
 
 fn main() {
 	let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -20,9 +22,12 @@ fn handle_connection(mut stream: TcpStream) {
 	stream.read(&mut buffer).unwrap();
 
 	let get = b"GET / HTTP/1.1\r\n";
-
+	let sleep = b"GET /sleep HTTP/1.1\r\n";
 	
 	let (status, content) = if buffer.starts_with(get) {
+		("HTTP/1.1 200 OK\r\n\r\n", &"Index.html")
+	} else if buffer.starts_with(sleep) {
+		thread::sleep(Duration::from_secs(5));
 		("HTTP/1.1 200 OK\r\n\r\n", &"Index.html")
 	} else {
 		("HTTP/1.1 404 NOT FOUND\r\n\r\n", &"Error_404.html")
