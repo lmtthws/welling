1a5b06238e721ec653bced7edde8f88b891d6460
Luke Matthews <lmtthws@gmail.com>
2018-08-18 13:57:01 -0500

    getting database back onto remote

--- web_server/src/http/http_context.rs
+++ web_server/src/http/http_context.rs
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 
 use http::parser::HttpRequestParser;
 use http::model::*;
@@ -33,4 +34,41 @@ impl HttpContext {
 			self.stream.flush().unwrap();
 	}
 
+=======
+
+use http::parser::HttpRequestParser;
+use http::request::StartLine;
+use std::net::TcpStream;
+use std::io::Write;
+
+//TODO: use a std::io::BufReader
+
+
+pub struct HttpContext {
+    stream: TcpStream,
+    parser: HttpRequestParser
+}
+
+impl HttpContext {
+    pub fn from_stream(stream: TcpStream) ->HttpContext {
+        HttpContext {
+            stream: stream,
+            parser: HttpRequestParser::new()
+        }
+    }
+
+    pub fn get_start_line(&mut self) -> Result<StartLine, (usize,String)> {
+        self.parser.get_start_line(&mut self.stream)
+    }
+
+    pub fn flush_request(&mut self) {
+        self.parser.get_request_body(&mut self.stream)
+    }
+
+    pub fn send_response(&mut self, response: String) {
+			self.stream.write(response.as_bytes()).unwrap();
+			self.stream.flush().unwrap();
+	}
+
+>>>>>>> Stashed changes
 }
--- web_server/src/http/mod.rs
+++ web_server/src/http/mod.rs
@@ -1,3 +1,9 @@
+<<<<<<< Updated upstream
 pub(crate) mod parser;
 pub(crate) mod http_context;
 pub(crate) mod model;
+=======
+pub(crate) mod request;
+pub(crate) mod parser;
+pub(crate) mod http_context;
+>>>>>>> Stashed changes
